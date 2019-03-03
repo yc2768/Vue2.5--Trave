385fa1d83858a0ab864c700bd1fbba5970df31c2
yc2768 <2768647687@qq.com>
2019-03-04 07:25:32 +0800

    使用iconfont,优化路径，使用stylus文件
    使用iconfont,优化路径，使用stylus文件，简化路径在webpack.base.conf.js中reslove配

--- vue2.5去哪儿/traveproject/trave/src/pages/home/components/Header.vue
+++ vue2.5去哪儿/traveproject/trave/src/pages/home/components/Header.vue
@@ -1,8 +1,16 @@
 <template>
     <div class="header">
-       <div class="header-left">返回</div>
-       <div class="header-input">输入城市/景点/游玩主题</div>
-       <div class="header-right">城市</div>
+       <div class="header-left">
+         <span class="iconfont back-icon" >&#xe624;</span>
+       </div>
+       <div class="header-input">
+         <span class="iconfont">&#xe632;</span>
+         输入城市/景点/游玩主题
+       </div>
+       <div class="header-right">
+         城市
+         <span class="iconfont arrow-icon">&#xe64a;</span>
+       </div>
     </div>
 </template>
 
@@ -14,14 +22,18 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+    @import '~styles/varibles.styl'
     .header
         display :flex
         line-height : .86rem
-        background : #00bcd4
+        background : $bgColor
         color: #fff
         .header-left
              width : .64rem
              float: left
+        .back-icon
+             text-align :center
+             font-size : .4rem
         .header-input
             flex: 1
             height: .64rem
@@ -36,5 +48,7 @@ export default {
             width : 1.24rem
             float : right
             text-align : center
-
+        .arrow-icon
+            font-size : .24rem
+            margin-left : -0.04rem
 </style>
