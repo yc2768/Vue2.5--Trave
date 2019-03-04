7739ef88207110854c68f2364a27da9b6070180c
yc2768 <2768647687@qq.com>
2019-03-05 07:15:37 +0800

    添加swiper 轮播组件
    添加swiper 轮播组件

--- vue2.5去哪儿/traveproject/trave/src/main.js
+++ vue2.5去哪儿/traveproject/trave/src/main.js
@@ -1,13 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import App from './App'
 import router from './router'
 import 'styles/reset.css'
 import 'styles/border.css'
 import 'styles/iconfont.css'
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
+Vue.use(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
 new Vue({
--- vue2.5去哪儿/traveproject/trave/src/pages/home/Home.vue
+++ vue2.5去哪儿/traveproject/trave/src/pages/home/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <home-header></home-header>
+    <home-swiper></home-swiper>
   </div>
 
 </template>
@@ -8,10 +9,12 @@
 <script>
 // 将局部组件HomeHeader引入到Home中并注册使用
 import HomeHeader from './components/Header'
+import HomeSwiper from './components/Swiper'
 export default {
   name: 'Home',
   components: {
-    HomeHeader
+    HomeHeader,
+    HomeSwiper
   }
 }
 </script>
