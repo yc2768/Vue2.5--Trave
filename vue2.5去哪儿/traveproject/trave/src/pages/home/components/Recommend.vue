0b5f1d69b86014652f163292a213dc82e4a1900b
yc2768 <2768647687@qq.com>
2019-03-06 07:36:07 +0800

    完成Recommend页面开发
    完成推荐列表组件开发

---
+++ vue2.5去哪儿/traveproject/trave/src/pages/home/components/Recommend.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+    <div class="title">
+      热销推荐
+    </div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <img class="item-img" :src="item.imgUrl"/>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}}</p>
+          <p class="item-desc">{{item.desc}}}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data: function () {
+    return {
+      recommendList: [{
+        id: '001',
+        imgUrl: '../../../../static/img/3.png',
+        title: '海洋馆',
+        desc: '浪漫的海洋馆'
+      }, {
+        id: '002',
+        imgUrl: '../../../../static/img/4.png',
+        title: '海洋馆',
+        desc: '浪漫的海洋馆'
+      }, {
+        id: '003',
+        imgUrl: '../../../../static/img/5.png',
+        title: '海洋馆',
+        desc: '浪漫的海洋馆'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~styles/mixins.styl"
+  .title
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+
+  .item
+    overflow: hidden
+    display: flex
+    height: 1.9rem
+
+  .item-img
+    width: 1.7rem
+    height: 1.7rem
+    padding: .1rem
+
+  .item-info
+    flex: 1
+    padding: .1rem
+
+  .item-title
+    line-height: .54rem
+    font-size: .32rem
+
+  .item-desc
+    line-height:, 4rem
+    color: #ccc
+
+  .item-button
+    line-height: .44rem
+    margin-top: .16rem
+    background: #ff9300
+    padding: 0 .2rem
+    border-radius: .06rem
+    color: #fff
+
+</style>
