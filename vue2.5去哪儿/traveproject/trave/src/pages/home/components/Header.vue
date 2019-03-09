fd51bd7d682d14e16a8a893837e815b154845512
yc2768 <2768647687@qq.com>
2019-03-09 16:22:32 +0800

    City页面Header和Input组件部分
    City页面Header和Input组件部分实现

--- vue2.5去哪儿/traveproject/trave/src/pages/home/components/Header.vue
+++ vue2.5去哪儿/traveproject/trave/src/pages/home/components/Header.vue
@@ -7,10 +7,12 @@
          <span class="iconfont">&#xe632;</span>
          输入城市/景点/游玩主题
        </div>
+       <router-link to="/city">
        <div class="header-right">
          {{this.city}}
          <span class="iconfont arrow-icon">&#xe64a;</span>
        </div>
+       </router-link>
     </div>
 </template>
 
@@ -27,7 +29,7 @@ export default {
     @import '~styles/varibles.styl'
     .header
         display :flex
-        line-height : .86rem
+        line-height : $headerHeight
         background : $bgColor
         color: #fff
         .header-left
@@ -50,6 +52,7 @@ export default {
             width : 1.24rem
             float : right
             text-align : center
+            color: #ffffff
         .arrow-icon
             font-size : .24rem
             margin-left : -0.04rem
