fd1388f09b180a4a809a9fd34d8221f0e4df7a80
yc2768 <2768647687@qq.com>
2019-03-10 20:38:20 +0800

    完成点击右边字母左边跳转
    完成点击右边字母左边跳转，兄弟组件之间传值

--- vue2.5去哪儿/traveproject/trave/src/pages/city/components/Alphabet.vue
+++ vue2.5去哪儿/traveproject/trave/src/pages/city/components/Alphabet.vue
@@ -1,6 +1,15 @@
 <template>
   <ul class="list">
-    <li class="item" v-for="(item ,key) of cities" :key="key">{{key}}</li>
+    <li class="item"
+        v-for="item  of letters"
+        :key="item"
+        :ref="item"
+        @touchstart.prevent="handleTouchStart"
+        @touchmove="handleTouchMove"
+        @touchend="handleTouchEnd"
+        @click="handleLetterClick">
+      {{item}}
+    </li>
   </ul>
 </template>
 
@@ -9,6 +18,48 @@ export default {
   name: 'CityAlphabet',
   props: {
     cities: Object
+  },
+  data () {
+    return {
+      touchStatus: false,
+      startY: 0,
+      timer: null
+    }
+  },
+  computed: {
+    letters () {
+      const letters = []
+      for (let i in this.cities) {
+        letters.push(i)
+      }
+      return letters
+    }
+  },
+  updated () {
+    this.startY = this.$ref['A'][0].offsetTop
+  },
+  methods: {
+    handleLetterClick (e) {
+      this.$emit('change', e.target.innerText)
+    },
+    handleTouchStart () {
+      this.touchStatus = true
+    },
+    handleTouchMove (e) {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      this.timer = setTimeout(() => {
+        const touchY = e.touches[0].clientY - 79
+        const index = Math.floor((touchY - this.startY) / 20)
+        if (index >= 0 && index < this.letters.length) {
+          this.$emit('change', this.letters[index])
+        }
+      }, 16)
+    },
+    handleTouchEnd () {
+      this.touchStatus = false
+    }
   }
 }
 </script>
