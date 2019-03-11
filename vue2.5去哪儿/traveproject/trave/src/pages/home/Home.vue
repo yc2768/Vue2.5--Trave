d950273cb6e977dc5b9011c20ea405ebfcbb9bde
yc2768 <2768647687@qq.com>
2019-03-11 22:34:27 +0800

    keep-alive 缓存性能优化
    keep-alive 缓存性能优化

--- vue2.5去哪儿/traveproject/trave/src/pages/home/Home.vue
+++ vue2.5去哪儿/traveproject/trave/src/pages/home/Home.vue
@@ -17,7 +17,7 @@ import HomeIcons from './components/Icons'
 import HomeRecommend from './components/Recommend'
 import HomeWeekend from './components/Weekend'
 import axios from 'axios'
-
+import { mapState } from 'vuex'
 export default {
   name: 'Home',
   components: {
@@ -35,9 +35,12 @@ export default {
       weekendList: []
     }
   },
+  computed: {
+    ...mapState(['city'])
+  },
   methods: {
     getHomeInfo () {
-      axios.get('/api/index.json')
+      axios.get('/api/index.json?city=' + this.city)
         .then(this.getHomeInfoSucc)
     },
     getHomeInfoSucc (res) {
@@ -56,9 +59,15 @@ export default {
     }
   },
   mounted () {
+    this.lastCity = this.city
     this.getHomeInfo()
+  },
+  activated () {
+    if (this.lastCity !== this.city) {
+      this.lastCity = this.city
+      this.getHomeInfo()
+    }
   }
-
 }
 </script>
 
